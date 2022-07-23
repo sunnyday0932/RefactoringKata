5a6b6f9942a0521419f926e8bcb52098ae83cc76
sunnyday0932 <sunnyday0932>
2022-07-23 22:22:39 +0800

    [Feat] FirstTest Case

---
+++ CSharp/Kata.Test/GlobalTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using RefactorKata;
+
+namespace Kata.Test
+{
+    [TestFixture]
+    public class GlobalTest
+    {
+        private BookmarkProcess _bookmarkProcess;
+        [SetUp]
+        public void SetUp()
+        {
+            _bookmarkProcess = new BookmarkProcess();
+        }
+
+        [Test]
+        public void Comare()
+        {
+            _bookmarkProcess.TranseFerFile();
+
+            var acutual = TestTool.GetFileHash("bookmarks_clear.txt");
+            var expected = TestTool.GetFileHash("expectedbookmarks.txt");
+
+            acutual.Should().Be(expected);
+        }
+    }
+}
